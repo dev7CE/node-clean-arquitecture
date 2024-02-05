9226dc32079cd6acd90dbc6dec68e3908a816dd2
ByronLeal <dev7CE@hotmail.com>
2024-02-05 11:37:34 -0600

    Create Express Server

--- node-clean-arquitecture/src/app.ts
+++ node-clean-arquitecture/src/app.ts
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+import { Server } from "./presentation/server";
+
+(() => {
+    main();
+})();
+
+async function main() {
+    // TODO: await Database
+    // TODO: start express.js server
+
+    console.log('console.log from main function');
+
+    new Server().start();
+}
