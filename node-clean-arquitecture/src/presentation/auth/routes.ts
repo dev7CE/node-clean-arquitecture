31416c39a84bed728254f4b37bf74feeaacf634d
ByronLeal <dev7CE@hotmail.com>
2024-02-06 14:02:39 -0600

    Consume repository with Dependency Injection
    - [AuthController]: Refactor contructor with authRepository as dependency. At this point, controller has any knwoledge about what Database is been used.
    - [AuthRoutes]: Refactor static get() by declare AuthSource Implementation and AuthRepository Implementation, then passing to AuthController as dependency.

--- node-clean-arquitecture/src/presentation/auth/routes.ts
+++ node-clean-arquitecture/src/presentation/auth/routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import { AuthController } from "./controller";
+import { AuthDatasourceImpl, AuthRepositoryImp } from "../../infrastructure";
 
 export class AuthRoutes {
     static get routes(): Router{
         const router = Router();
-        const controller = new AuthController();
+
+        // Inject Depedency into AuthController
+        const database = new AuthDatasourceImpl();
+        const authRepository = new AuthRepositoryImp(database);
+
+        const controller = new AuthController(authRepository);
 
         // Controller Routes Definition
         router.post('/login', controller.loginUser);
