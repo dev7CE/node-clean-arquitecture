faca3dbb4480b2b0145a92a3676dde7380f340d9
ByronLeal <dev7CE@hotmail.com>
2024-02-06 12:57:36 -0600

    Datasources and repositories

---
+++ node-clean-arquitecture/src/domain/datasources/auth/auth.datasource.ts
@@ -0,0 +1,9 @@
+import { RegisterUserDto } from "../../dtos/auth/register-user-dto";
+import { UserEntity } from "../../entities/user.entitity";
+
+export abstract class AuthDatasource {
+    // TODO: 
+    // abstract login (loginUserDto: LoginUserDto): Promise<UserEntity>
+
+    abstract register(registerUserDto: RegisterUserDto):Promise<UserEntity>
+}
