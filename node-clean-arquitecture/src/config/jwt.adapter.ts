6e4e46f0e9b8fd20f44c15b02bd28a5e93355dfe
ByronLeal <dev7CE@hotmail.com>
2024-02-11 14:04:50 -0600

    Implement configuration for Json Web Token (JWT)
    - Use Promise for return JWT.
    - [AuthController]: refactor json response on 'registerUser', return user entity plus generated token.

--- node-clean-arquitecture/src/config/jwt.adapter.ts
+++ node-clean-arquitecture/src/config/jwt.adapter.ts
@@ -1,7 +1,16 @@
 import jwt from 'jsonwebtoken';
 
 export class JwtAdapter {
-    static async generateToken(payload: Object, duration: string = '2h') {
-        
+    static async generateToken(payload: Object, duration: string = '2h'): Promise<string|null> {
+        return new Promise( (resolve) => {
+            // TODO: generate SEED
+            jwt.sign(payload, 'SEED', { expiresIn: duration}, (err, token) => {
+                if (err) {
+                    return resolve(null);
+                }
+
+                resolve(token!);
+            });
+        });
     }
 }
--- node-clean-arquitecture/src/presentation/auth/controller.ts
+++ node-clean-arquitecture/src/presentation/auth/controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import { AuthRepository, CustomError, RegisterUserDto } from '../../domain';
+import { JwtAdapter } from '../../config';
+
 export class AuthController {
     //DI
     constructor (
@@ -25,7 +27,14 @@ export class AuthController {
         if (error) return res.status(400).json({ error });
 
         this.authRepository.register(registerUserDto!)
-            .then(user => res.json(user))
+            .then(async (user) => {
+                res.json({
+                    user,
+                    token: await JwtAdapter.generateToken({ 
+                        id: user.id 
+                    })
+                });
+            })
             .catch(error => this.handleError(error, res));
     }
 
