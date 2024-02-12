15ce2b87eb67555de9bbc6d8c4e6215a9dc314f1
ByronLeal <dev7CE@hotmail.com>
2024-02-11 23:04:25 -0600

    Generate JWT SEED
    - Generate random hexadecimal characters using Linux command to use on new environment variable.
    - Create new environment variable JWT_SEED.
    (Note: this value might based on .env.template or generated using openssl commad)
    - Map new environment variable into env  adapter.
    - [JwtAdapter]: Make JWT_SEED const as explicit dependency.
    - [JwtAdapter]: pass const JWT_SEED into 'sing()' and 'verify()' jwt method.

--- node-clean-arquitecture/src/config/jwt.adapter.ts
+++ node-clean-arquitecture/src/config/jwt.adapter.ts
@@ -1,11 +1,13 @@
 import jwt from 'jsonwebtoken';
-import { resolve } from 'path';
+import { envs } from './envs';
+
+const JWT_SEED = envs.JWT_SEED;
 
 export class JwtAdapter {
     static async generateToken(payload: Object, duration: string = '2h'): Promise<string|null> {
         return new Promise( (resolve) => {
             // TODO: generate SEED
-            jwt.sign(payload, 'SEED', { expiresIn: duration}, (err, token) => {
+            jwt.sign(payload, JWT_SEED, { expiresIn: duration}, (err, token) => {
                 if (err) {
                     return resolve(null);
                 }
@@ -17,7 +19,7 @@ export class JwtAdapter {
 
     static async validateToken<T>(token: string): Promise<T | null> {
         return new Promise( (resolve) => {
-            jwt.verify(token, 'SEED', (err, decoded) => {
+            jwt.verify(token, JWT_SEED, (err, decoded) => {
                 if (err) {
                     return resolve(null);
                 }
