5a12c171ce5029acd49dd4455249532b4e743531
ByronLeal <dev7CE@hotmail.com>
2024-02-12 10:55:48 -0600

    Implement register Use Case: Refatort AuthContrllr
    - Export register-user.use-case to barrel file
    - [AuthController]: Remove initial implementation and call RegisterUser User Case.

--- node-clean-arquitecture/src/domain/index.ts
+++ node-clean-arquitecture/src/domain/index.ts
@@ -3,3 +3,4 @@ export * from './entities/user.entitity';
 export * from './errors/custom.error';
 export * from './datasources/auth/auth.datasource';
 export * from './repositories/auth.repository';
+export * from './use-cases/auth/register-user.use-case';
--- node-clean-arquitecture/src/presentation/auth/controller.ts
+++ node-clean-arquitecture/src/presentation/auth/controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { AuthRepository, CustomError, RegisterUserDto } from '../../domain';
+import { AuthRepository, CustomError, RegisterUser, RegisterUserDto } from '../../domain';
 import { JwtAdapter } from '../../config';
 import { UserModel } from '../../data/mongodb';
 
@@ -27,16 +27,8 @@ export class AuthController {
 
         if (error) return res.status(400).json({ error });
 
-        // NOTE: Part of this logic was taken for Use Case Implementation
-        this.authRepository.register(registerUserDto!)
-            .then(async (user) => {
-                res.json({
-                    user,
-                    token: await JwtAdapter.generateToken({ 
-                        id: user.id 
-                    })
-                });
-            })
+        new RegisterUser(this.authRepository).execute(registerUserDto!)
+            .then(data => res.json(data))
             .catch(error => this.handleError(error, res));
     }
 
