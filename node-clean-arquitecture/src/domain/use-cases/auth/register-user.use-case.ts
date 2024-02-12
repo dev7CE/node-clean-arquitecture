3eaa8c8cd52284e17d3159339139d861828ecdd2
ByronLeal <dev7CE@hotmail.com>
2024-02-12 10:49:02 -0600

    Implement register Use Case: Generate Token

--- node-clean-arquitecture/src/domain/use-cases/auth/register-user.use-case.ts
+++ node-clean-arquitecture/src/domain/use-cases/auth/register-user.use-case.ts
@@ -1,4 +1,6 @@
+import { JwtAdapter } from "../../../config";
 import { RegisterUserDto } from "../../dtos/auth/register-user-dto";
+import { CustomError } from "../../errors/custom.error";
 import { AuthRepository } from "../../repositories/auth.repository";
 
 interface RegisterUserUseCase {
@@ -14,23 +16,30 @@ interface UserToken {
     };
 }
 
+type SignToken = (payload: Object, duration?: string) => Promise<string | null>
+
 export class RegisterUser implements RegisterUserUseCase {
     constructor(
         private readonly authRepository: AuthRepository,
+        private readonly signToken: SignToken = JwtAdapter.generateToken
     ){}
 
     async execute(registerUserDto: RegisterUserDto): Promise<UserToken> {
 
         const user = await this.authRepository.register(registerUserDto);
-        const token = '';
+        const token = await this.signToken({ id: user.id }, '2h');
+
+        if(!token) {
+            throw CustomError.internalServerError('Error generating token');
+        }
 
         return {
-            token: token,
             user: {
                 id: user.id,
                 name: user.name,
                 email: user.email,
-            }
+            },
+            token: token,
         };
     }
 }
